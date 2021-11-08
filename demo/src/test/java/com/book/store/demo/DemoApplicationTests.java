4c3ed806af3984ec4e76f292a8910a292d465b23
Mayssa_JAWADI <74791542+mayssajawadi@users.noreply.github.com>
2021-11-08 10:11:06 +0100

    Add files via upload

---
+++ demo/src/test/java/com/book/store/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.store.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
